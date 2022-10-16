26eae9c14192aa006c4f0aec7e6622afce13ef9c
RavindraNbiradar <ravibiradar028@gmail.com>
2022-10-16 13:48:22 +0530

    hard code expenses

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem() {
+    return <div>
+        <h2>Expense Items!</h2>
+    <p>food 10</p>
+    <p>petrol 100</p> 
+    <p>movies 200</p>  
+    </div>
+
+}
+export default ExpenseItem;
