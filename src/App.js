80e51c21cad0663ed22fafdd50db905e0e2bb545
RavindraNbiradar <ravibiradar028@gmail.com>
2022-11-01 18:33:25 +0530

    adding header form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
@@ -32,7 +33,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={items} deleteItem={deleteItem} />
     </div>
   );
