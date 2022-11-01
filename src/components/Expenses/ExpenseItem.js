debff114eab8e5dd39175890683e265f79f6831b
RavindraNbiradar <ravibiradar028@gmail.com>
2022-11-01 14:14:32 +0530

    Add Expense 100

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,26 @@
 import ExpenseDate from './ExpenseDate';
+import React, { useState } from 'react';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  const [amount,addamount]=useState(props.amount)
 
+  const addExpense=()=>{
+      addamount(amount+100);
+      
+  }
+  console.log(addExpense,'addexpense')
   
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className='expense-item__price'>${amount}</div>
       </div>
-      <button onClick={()=>props.delete(props.amount)}>delete</button>
-      <button onClick={()=>props.delete()}>update</button>
+      {/* <button onClick={()=>props.delete(props.amount)}>delete</button> */}
+      <button onClick={addExpense}>AddEXpense</button>
     </Card>
   );
 }
