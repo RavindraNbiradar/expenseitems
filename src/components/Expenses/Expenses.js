40ecf52972599785e06326d11eb8a72353956528
RavindraNbiradar <ravibiradar028@gmail.com>
2022-11-06 12:35:39 +0530

    final chart app

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,10 @@
-import ExpenseItem from "./ExpenseItem";
+
 import Card from "../UI/Card";
 import "./Expenses.css";
 import { useState } from "react";
 import ExpensesFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -13,18 +15,18 @@ function Expenses(props) {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  let expensesContent = <p>No expenses found.</p>;
+  //let expensesContent = <p>No expenses found.</p>;
 
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
+  // if (filteredExpenses.length > 0) {
+    // expensesContent = filteredExpenses.map((expense) => (
+      // <ExpenseItem
+        // key={expense.id}
+        // title={expense.title}
+        // amount={expense.amount}
+        // date={expense.date}
+      // />
+    // ));
+  // }
 
   return (
     <div>
@@ -33,7 +35,9 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-         {expensesContent}
+         {/* {expensesContent} */}
+         <ExpensesChart expenses={filteredExpenses} />
+         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
